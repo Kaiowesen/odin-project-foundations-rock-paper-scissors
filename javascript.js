bfee8781336efb3c0667d708f6faa4e514b12ca9
Kai Hansen <kaiowesen@gmail.com>
2022-06-16 22:44:57 +0200

    JS file has function to select random array. Does not select random yet.

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//Make a let function called computerplay and the constonants rock paper and scissor//
+
+
+
+    const myArray = [ "rock",  "paper", "scissor"];
+    let computerPlay = myArray[Math.floor(Math.random() * (myArray.length))];
+    console.log(computerPlay);
+   
+
+
+
+//Make array of three strings //
+//return random string in console//
