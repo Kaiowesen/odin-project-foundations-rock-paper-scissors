466c6f8caf551ce28d2aff316122ddba9d9e4c84
Kai Hansen <kaiowesen@gmail.com>
2022-06-23 21:26:21 +0200

    start of startGame function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,50 @@
-//Make a let function called computerplay and the constonants rock paper and scissor//
+//Make a let function called computerPlay and the constonnts rock paper and scissor
 
 
-
-    const myArray = [ "rock",  "paper", "scissor"];
-    let computerPlay = myArray[Math.floor(Math.random() * (myArray.length))];
+//Make array of three strings 
+    const myArray = [ "rock",  "paper", "scissors"];
+    let computerPlay = myArray[Math.floor(Math.random()*3)];
     console.log(computerPlay);
    
 
+//get user input
+function startGame {
+let toLowerCase.playerSelection = promt ("Let/'s play! Choose rock, paper or scissors)
+//pair playerSelection against computerPlay
+    if {
+        playerSelection != rock || paper || scissors;
+        console.log("Please choose rock paper or scissors");
+    if {
+        playerSelection === computerPlay;
+        console.log("It/'s a draw!");
+    }
+    if  {
+        playerSelection === rock && computerPlay === scissors
+        playerSelection > computerPlay
+    }
+    if  {
+        playerSelection === rock && computerPlay === paper;
+        playerSelection < computerPlay
+    }
+    if {
+        playerSelection === scissors && computerPlay === rock;
+        playerSelection < computerPlay
+    }
+    if {
+        playerSelection === scissors && computerPlay === paper;
+        playerSelection > computerPlay
+    }
+    if {
+        playerSelection === paper && computerPlay === rock;
+        playerSelection > computerPlay
+    }
+    if {
+        playerSelection === paper && computerPlay === scissors;
+        playerSelection < computerPlay
+    }
+    }
+    
+    
 
+}
 
-//Make array of three strings //
-//return random string in console//
