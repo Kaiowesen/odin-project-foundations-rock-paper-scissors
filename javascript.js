3fad2f8e8700f1111e85191374b0c886274cbec8
Kai Hansen <kaiowesen@gmail.com>
2022-06-28 07:05:39 +0200

    Still making a function that plays a round of rps

--- javascript.js
+++ javascript.js
@@ -7,19 +7,37 @@
     console.log(computerPlay);
    
 
+let playerchoice = promt ("Let/'s play! Choose rock, paper or scissors");
+const playerSelection = playerchoice.toLowerCase;
+const computerSelection = computerPlay();
 //get user input
-function startGame {
-let toLowerCase.playerSelection = promt ("Let/'s play! Choose rock, paper or scissors)
-//pair playerSelection against computerPlay
+function startGame(playerSelection, computerSelection) {
+    if playerSelection > computerSelection {
+    return ("Player wins")  
+    }
+    else if playerSelection < computerSelection {
+    return ("Computer wins")
+    }
+    else if playerSelection === computerSelection {
+    return ("it/'s a draw!")
+    else {
+    return ("something went wrong, please play again")
+    }
+}
+
+
+
+
     if {
         playerSelection != rock || paper || scissors;
         console.log("Please choose rock paper or scissors");
+    }
     if {
         playerSelection === computerPlay;
         console.log("It/'s a draw!");
     }
     if  {
-        playerSelection === rock && computerPlay === scissors
+        playerSelection === rock && computerPlay === scissors   
         playerSelection > computerPlay
     }
     if  {
@@ -42,9 +60,9 @@ let toLowerCase.playerSelection = promt ("Let/'s play! Choose rock, paper or sci
         playerSelection === paper && computerPlay === scissors;
         playerSelection < computerPlay
     }
-    }
+}
     
     
 
-}
+
 
