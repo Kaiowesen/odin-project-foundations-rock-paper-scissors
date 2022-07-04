62fde044e0ab1b75363c2766b649d49a58fab86b
Kai Hansen <kaiowesen@gmail.com>
2022-07-04 19:48:05 +0200

    Added new testet and working functions playerChoise and computerChoice

--- javascript.js
+++ javascript.js
@@ -1,68 +1,72 @@
-//Make a let function called computerPlay and the constonnts rock paper and scissor
+
+
+
+    
+  
+    
+    
+   
+   
+
 
 
 //Make array of three strings 
-    const myArray = [ "rock",  "paper", "scissors"];
-    let computerPlay = myArray[Math.floor(Math.random()*3)];
-    console.log(computerPlay);
+const myArray = [ "rock",  "paper", "scissors"];
+ 
+function playGame () {
+    playRound();
+    checkResult();
+}
+
+function playerChoice () {
+    let input = prompt ("Let/'s play! Choose rock, paper or scissors");
+    input = input.toLowerCase();
+    return input
+}
+
+
+function computerChoice () {
    
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
 
-let playerchoice = promt ("Let/'s play! Choose rock, paper or scissors");
-const playerSelection = playerchoice.toLowerCase;
-const computerSelection = computerPlay();
-//get user input
-function startGame(playerSelection, computerSelection) {
-    if playerSelection > computerSelection {
-    return ("Player wins")  
+
+function playRound() {
+   const playerSelection = playerChoice();
+   const computerSelection = computerChoice();
+   return (playerSelection, computerSelection);
+}
+
+function checkResult (playerSelection,computerSelection) {
+    if  (playerSelection != rock || paper || scissors) {
+        return ("Please choose rock paper or scissors");
     }
-    else if playerSelection < computerSelection {
-    return ("Computer wins")
+    else if (playerSelection === computerSelection) {
+        return ("it/'s a draw!");
     }
-    else if playerSelection === computerSelection {
-    return ("it/'s a draw!")
+    else if (playerSelection === computerSelection) {
+        return ("It/'s a draw!");
+    }
+    else if  (playerSelection === rock && computerSelection === scissors) {  
+        return ("rock beats scissors, Player wins!");
+    }
+    else if  (playerSelection === rock && computerSelection === paper) {
+        return ("paper beats rock. Computer wins!");
+    }
+    else if (playerSelection === scissors && computerSelection === rock){
+        return ("rock beats scissors, computer wins!");
+    }
+    else if (playerSelection === scissors && computerSelection === paper) {
+        return ("scissors beats paper. Player wins!");
+    }
+    else if (playerSelection === paper && computerSelection === rock) {
+        return ("paper beats rock. Player wins!");
+    }
+    else if (playerSelection === paper && computerSelection === scissors) {
+        return ("scissors beats paper. Player wins!");
+    }
     else {
-    return ("something went wrong, please play again")
+    return ("something went wrong, please play again");
     }
 }
-
-
-
-
-    if {
-        playerSelection != rock || paper || scissors;
-        console.log("Please choose rock paper or scissors");
-    }
-    if {
-        playerSelection === computerPlay;
-        console.log("It/'s a draw!");
-    }
-    if  {
-        playerSelection === rock && computerPlay === scissors   
-        playerSelection > computerPlay
-    }
-    if  {
-        playerSelection === rock && computerPlay === paper;
-        playerSelection < computerPlay
-    }
-    if {
-        playerSelection === scissors && computerPlay === rock;
-        playerSelection < computerPlay
-    }
-    if {
-        playerSelection === scissors && computerPlay === paper;
-        playerSelection > computerPlay
-    }
-    if {
-        playerSelection === paper && computerPlay === rock;
-        playerSelection > computerPlay
-    }
-    if {
-        playerSelection === paper && computerPlay === scissors;
-        playerSelection < computerPlay
-    }
-}
-    
-    
-
-
-
+playGame();
