fc58b1cf59266ffea10f90ff0c16dae2ae65d03d
Kai Hansen <kaiowesen@gmail.com>
2022-07-05 20:31:41 +0200

    Working playRound function that completes a round

--- javascript.js
+++ javascript.js
@@ -12,16 +12,14 @@
 
 //Make array of three strings 
 const myArray = [ "rock",  "paper", "scissors"];
- 
-function playGame () {
-    playRound();
-    checkResult();
-}
-
+ function playGame() {
+let result = playRound();
+console.log(result);
+ }
 function playerChoice () {
     let input = prompt ("Let/'s play! Choose rock, paper or scissors");
-    input = input.toLowerCase();
-    return input
+    input = input.toLowerCase(); 
+    return input;
 }
 
 
@@ -34,39 +32,39 @@ function computerChoice () {
 function playRound() {
    const playerSelection = playerChoice();
    const computerSelection = computerChoice();
-   return (playerSelection, computerSelection);
+   const winner = checkResult(playerSelection, computerSelection);
+   console.log(winner);
 }
 
+
+
 function checkResult (playerSelection,computerSelection) {
-    if  (playerSelection != rock || paper || scissors) {
-        return ("Please choose rock paper or scissors");
-    }
-    else if (playerSelection === computerSelection) {
+    if (playerSelection == computerSelection) {
         return ("it/'s a draw!");
     }
-    else if (playerSelection === computerSelection) {
+    else if (playerSelection == computerSelection) {
         return ("It/'s a draw!");
     }
-    else if  (playerSelection === rock && computerSelection === scissors) {  
+    else if  (playerSelection == "rock" && computerSelection == "scissors") {  
         return ("rock beats scissors, Player wins!");
     }
-    else if  (playerSelection === rock && computerSelection === paper) {
+    else if  (playerSelection == "rock" && computerSelection == "paper") {
         return ("paper beats rock. Computer wins!");
     }
-    else if (playerSelection === scissors && computerSelection === rock){
+    else if (playerSelection == "scissors" && computerSelection == "rock"){
         return ("rock beats scissors, computer wins!");
     }
-    else if (playerSelection === scissors && computerSelection === paper) {
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
         return ("scissors beats paper. Player wins!");
     }
-    else if (playerSelection === paper && computerSelection === rock) {
+    else if (playerSelection == "paper" && computerSelection == "rock") {
         return ("paper beats rock. Player wins!");
     }
-    else if (playerSelection === paper && computerSelection === scissors) {
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
         return ("scissors beats paper. Player wins!");
     }
     else {
     return ("something went wrong, please play again");
     }
 }
-playGame();
+playRound();
