50953506c59446acc18ea6684eeba8cc7b646955
Kai Hansen <kaiowesen@gmail.com>
2022-07-25 20:04:16 +0200

    working game that plays five rounds. Still need to add function to console.log results

--- javascript.js
+++ javascript.js
@@ -2,69 +2,105 @@
 
 
     
-  
-    
-    
-   
-   
-
-
-
-//Make array of three strings 
 const myArray = [ "rock",  "paper", "scissors"];
- function playGame() {
-let result = playRound();
-console.log(result);
- }
+const winners = [];
+let playerWins = [];
+let computerWins = [];
+let drawsArray = [];
+    
+function game () {
+    i=playRound();
+    for (let i = 1; i < 5; i++) {
+        playRound();
+        console.log(winners);  
+     };
+}
+    
+
+
+ //gets input from user, missing 
 function playerChoice () {
     let input = prompt ("Let/'s play! Choose rock, paper or scissors");
-    input = input.toLowerCase(); 
+    input = validationCheck(input);
     return input;
 }
 
 
+
+//gets random input from computer
 function computerChoice () {
    
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-
+//function to play one round and announce winner
 function playRound() {
    const playerSelection = playerChoice();
    const computerSelection = computerChoice();
    const winner = checkResult(playerSelection, computerSelection);
-   console.log(winner);
-}
+   winners.push(winner)
+   playerWins = winners.filter(function(item) {
+    return (item == "player");
+    });
+    computerWins = winners.filter(function(item) {
+    return (item == "computer");
+    });
+  drawsArray = drawsArray = winners.filter(function(item) {
+    return (item == "draw");
+    });
+};
 
 
 
+
+//function to check who wins and prints result
 function checkResult (playerSelection,computerSelection) {
-    if (playerSelection == computerSelection) {
-        return ("it/'s a draw!");
-    }
-    else if (playerSelection == computerSelection) {
-        return ("It/'s a draw!");
+     if (playerSelection == computerSelection) {
+        return ("draw");
     }
     else if  (playerSelection == "rock" && computerSelection == "scissors") {  
-        return ("rock beats scissors, Player wins!");
+        return ("player");
     }
     else if  (playerSelection == "rock" && computerSelection == "paper") {
-        return ("paper beats rock. Computer wins!");
+        return ("computer");
     }
     else if (playerSelection == "scissors" && computerSelection == "rock"){
-        return ("rock beats scissors, computer wins!");
+        return ("computer");
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return ("scissors beats paper. Player wins!");
+        return ("player");
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        return ("paper beats rock. Player wins!");
+        return ("player");
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return ("scissors beats paper. Player wins!");
+        return ("computer");
     }
     else {
     return ("something went wrong, please play again");
     }
 }
-playRound();
+
+function validationCheck (answer) {
+   let _answer = answer
+   answer = answer.toLowerCase();
+    while (answer != "rock" && answer != "paper" && answer != "scissors")  {
+       answer = prompt ("Choose rock, paper or scissors, spelling needs to be excact! Please play again!");
+    } 
+   return answer;
+ };
+
+ function roundScore (roundWinner) {
+    if (roundWinner == "player") {
+        console.log("player wins round")
+    }
+    else if (roundWinner == "computer") {
+        console.log("computer wins round")
+    }
+    else if (roundWinner == "draw") {
+            console.log("It/'s a draw!");
+        }
+
+ }
+ 
+ game();
