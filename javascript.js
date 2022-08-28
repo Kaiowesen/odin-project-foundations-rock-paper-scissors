9cc8dadeea933543b2cadb87c81a502f589fde3d
Kai Hansen <kaiowesen@gmail.com>
2022-08-28 20:08:30 +0200

    Working game playing five rounds!

--- javascript.js
+++ javascript.js
@@ -7,7 +7,10 @@ const winners = [];
 let playerWins = [];
 let computerWins = [];
 let drawsArray = [];
-    
+let playerSelect = "";
+let resultDiv = document.getElementById("divR")
+let rounDiv = document.getElementById("divC")
+let counter = 0;
 /*function game () {
     for (let i = 0; i < 5; i++) {
         playRound();
@@ -18,40 +21,13 @@ let drawsArray = [];
 }
 */
     
-
-
- //gets input from user, missing 
-/*function playerChoice () {
- if ('click' == btnP) return "paper";
- if ('click' == btnR) return "rock"
- if ('click' == btnS) return "scissors"
-
-}*/
-
-
-
 //gets random input from computer
 function computerChoice () {
    
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-//function to play one round and announce winner
-function playRound() {
-   const playerSelection = playerSelect;
-   const computerSelection = computerChoice();
-   const winner = checkResult(playerSelection, computerSelection);
-   winners.push(winner)
-   playerWins = winners.filter(function(item) {
-    return (item == "player");
-    });
-    computerWins = winners.filter(function(item) {
-    return (item == "computer");
-    });
-  drawsArray = drawsArray = winners.filter(function(item) {
-    return (item == "draw");
-    });
-};
+
 
 
 
@@ -66,7 +42,7 @@ function checkResult (playerSelection,computerSelection) {
     
 }
 
-function validationCheck (answer) {
+/*function validationCheck (answer) {
    let _answer = answer
    while (answer == null) {
     answer = prompt ("Choose rock, paper or scissors");
@@ -77,23 +53,36 @@ function validationCheck (answer) {
     } 
    return answer;
  };
+ */
 
 
 
 function finalScore (winP,winC) {
-         if (winP.length > winC.length || winC == null) {
-            console.log("The final score is player " + winP.length + " and computer " + winC.length + " player wins!");
-        }
-         else if (winC.length > winP.length || winP == null) {
-            console.log("The final score is computer " + winC.length + " and player " + winP.length + " computer wins!");
-        }
-         else {
-            console.log("The final score is computer " + winC.length + " and player " + winP.length + " It\'s a draw!");
-        }
-    
-    }
+        if ((winP.length + winC.length + drawsArray.length) == 5 && winP.length > winC.length) {
+            alert("Game over Final result is Player " + winP.length + " Computer " + winC.length +  " player wins! refresh page to play again")
+            stop();
+            
+        } 
+   
 
-    function roundScore (roundScore) {
+         else if ((winP.length + winC.length + drawsArray.length) == 5 && winC.length > winP.length) {
+            alert("Game over Final result is Computer " + winC.length + ", player " + winP.length + " and draws " + drawsArray.length + " computer wins! refresh page to play again")
+            stop();
+        }
+        else if ((winP.length + winC.length + drawsArray.length) == 5 && winC.length == winP.length) {
+            alert("The score is computer " + winC.length + ", player " + winP.length + " and draws " + drawsArray.length + " It\'s a draw! refresh page to play again")
+            stop();
+        }
+        else if (winC.length && winP.length == null)   {
+            alert("The score is computer 0 and Player 0. It/'s a draw!")
+        }
+
+         else {
+            resultDiv.textContent = "The score is computer " + winC.length + ", player " + winP.length + " and draws " + drawsArray.length;
+        }
+}
+
+    /*function roundScore (roundScore) {
         if (roundScore == "player") {
             console.log("player wins round")
         }
@@ -103,16 +92,44 @@ function finalScore (winP,winC) {
         else if (roundScore == "draw") {
                 console.log("It\'s a draw!");
             }
-        }
+        }*/
 
-        const buttons = document.querySelectorAll('button');
+       const buttons = document.querySelectorAll('button');
         
         buttons.forEach((button) => {
 
             // and for each one we add a 'click' listener
             button.addEventListener('click', () => {
-              let playerSelects = (button.id);
-              console.log(playerSelects)
+                playerSelect = (button.id)
+                counter++;
+                if (counter < 6){
+                    playRound();    
+                    finalScore(computerWins, playerWins);
+                }
+              
             });
-          });
- 
+        });
+           
+            //playRound();
+            //finalScore(computerWins, playerWins);
+            //});
+    
+          
+
+//function to play one round and announce winner
+function playRound() {
+    const computerSelection = computerChoice();
+    const winner = checkResult(playerSelect, computerSelection);
+    rounDiv.textContent = " player chooses " + playerSelect + " and computer chooses " + computerSelection;
+    winners.push(winner);
+    playerWins = winners.filter(function(item) {
+        return (item == "player");
+        });
+    computerWins = winners.filter(function(item) {
+        return (item == "computer");
+        });
+    drawsArray = winners.filter(function(item) {
+        return (item == "draw");
+        });
+
+ }
